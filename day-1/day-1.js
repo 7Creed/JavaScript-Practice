aab8099011268273e4b8a30d7f7b510d136babf5
7Creed <4chimabika@gmail.com>
2023-02-16 21:02:24 +0100

    JavaScript-Practice

---
+++ day-1/day-1.js
@@ -0,0 +1,66 @@
+// Q/N  1.
+// comments can make code readable
+
+// Q/N  2.
+// Welcome to 30DaysOfJavaScript
+
+// Q/N  3.
+/**
+ * comments can make code readable,
+ * easy to reuse and informative
+ */
+
+// Q/N  4.
+let username = 'creed';
+let isMarried = true;
+let notValid = undefined;
+let not = null;
+// let emptyValue = null
+
+// Q/N  5.
+console.log(typeof 'creed') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+
+// console.log(typeof username) // string
+// console.log(typeof isMarried) // boolean
+// console.log(typeof notValid) // undefined
+// console.log(typeof not) // null
+
+
+// Q/N  6.
+let one;
+let two;
+let three;
+let four;
+
+// Q/N  7.
+let a = "Addition";
+let b = "Subtraction";
+let c = "Multiplication";
+let d = "Division";
+
+// Q/N  8.
+// Declaring variables in multiple line
+let firstName_1 = "Chima";
+let lastName_1 = "Obika";
+let isMarried_1 = true;
+let country_1 = "Nigeria";
+let age_1 = 50;
+
+// Q/N  9.
+// Declaring variables in a single line
+let firstName_2 = "Chima",
+  lastName_2 = "Obika",
+  isMarried_2 = true,
+  country_2 = "Nigeria",
+  age_2 = 50;
+
+  // Q/N  10.
+let myAge = 50;
+let yourAge = 100;
+
+console.log(`I am ${myAge} years old.`);
+console.log(`You are ${yourAge} years old.`);
---
+++ variable.js
@@ -0,0 +1,10 @@
+let username = 'creed';
+let isMarried = true;
+let notValid = undefined;
+let not = null;
+// let emptyValue = null
+
+// console.log(typeof username) // string
+// console.log(typeof isMarried) // boolean
+// console.log(typeof notValid) // undefined
+// console.log(typeof not) // null
